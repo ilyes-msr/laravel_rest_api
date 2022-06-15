a12d0ba3aaa11836c2e25492fbc780a9a0c4cf22
Ali Ilyes MANSOUR <mansour.aliilyes@gmail.com>
2022-06-15 13:04:16 +0100

    initial commit

---
+++ database/migrations/2022_06_04_140823_create_petitions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('petitions', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('category');
+            $table->text('description');
+            $table->string('author');
+            $table->integer('signees');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('petitions');
+    }
+};
---
+++ database/seeders/PetitionSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Petition;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PetitionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Petition::factory()->times(50)->create();
+    }
+}
